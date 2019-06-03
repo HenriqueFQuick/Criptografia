fc4cccb94f7f491d8ae74f1957e7869bd77ae6f5
SeraphyBR <luisjuniorbr@gmail.com>
2019-06-03 20:48:16 -0300

    update

--- Projeto.java
+++ Projeto.java
@@ -1,125 +1,139 @@
 import java.util.Scanner;
-import java.io.*;
-import java.util.Random;
-public class Projeto{
-   public static void main(String[] args) {
-      Scanner read = new Scanner(System.in);
-      boolean sair = false;
-      while(!sair){
-        try {
+import java.io.RandomAccessFile;
+import java.io.File;
+import java.security.SecureRandom;
+import java.util.InputMismatchException;
 
-            System.out.println("\n------------* MENU *------------\n0 - Escrever em um arquivo\n1 - Criptografar um arquivo\n2 - Descriptografar um arquivo\n3 - Sair do programa");
-            System.out.print("\nOpcao desejada: ");
-            int opcao = read.nextInt();
+public class Projeto
+{//Inicio classe Projeto
 
-            switch(opcao){
-               case 0:
-                  escreverArquivo();
-                  break;
-               case 1:
-                  criptografarArquivo();
-                  break;
-               case 2:
-                  descriptografarArquivo();
-                  break;
-               case 3:
-                  sair = true;
-                  break;
-               default:
-                  System.out.println("Opcao invalida");
-                  break;
+    private static final Scanner read = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        boolean sair = false;
+        while(!sair){
+            try {
+                System.out.println(
+                        "\n------------* MENU *------------\n" + 
+                        "0 - Escrever em um arquivo e criptografar\n" +
+                        "1 - Criptografar um arquivo\n" +
+                        "2 - Descriptografar um arquivo\n" +
+                        "3 - Sair do programa"
+                        );
+                System.out.print("\nOpcao desejada: ");
+                byte opcao = read.nextByte();
+                switch(opcao){
+                    case 0:
+                        escreverArquivo();
+                        break;
+                    case 1:
+                        criptografarArquivo();
+                        break;
+                    case 2:
+                        descriptografarArquivo();
+                        break;
+                    case 3:
+                        sair = true;
+                        break;
+                    default:
+                        System.out.println("Opcao invalida");
+                        break;
+                }
             }
-
-         }catch(Exception e) {
-             System.out.print("Erro: ");
-            e.printStackTrace();
-         }
-      }
-      read.close();
+            catch(InputMismatchException ime){
+                System.out.println("\nOops! Parece que digitou algo errado por engano!\nTente novamente!");
+                read.next(); // Limpa buffer do scanner
+            }
+            catch(Exception e) {
+                System.out.print("Erro: ");
+                e.printStackTrace();
+                sair = true;
+            }
+        }
+        read.close();
     } // fim do main()
 
-    public static void escreverArquivo()throws Exception{
-       Scanner read = new Scanner(System.in);
-       String nome = "";
-       System.out.print("Qual o nome do arquivo? ");
-       nome = read.nextLine();
-       RandomAccessFile raf = new RandomAccessFile(nome, "rw");
-       System.out.println("Digite o que deseja escrever no arquivo:");
-       String text = read.nextLine();
-       byte[] b = text.getBytes();
-       raf.read(b);
-       Random random = new Random();
-       int chave = random.nextInt(100);
-       System.out.println("Sua chave de criptografia eh: " + chave);
-       b = cifrar(b,chave);
-       raf.seek(0);
-       raf.writeInt(chave);
-       raf.write(b);
-       raf.close();
-       read.close();
+    public static void escreverArquivo() throws Exception{
+        String nome = "";
+        System.out.print("Qual o nome do arquivo? ");
+        nome = read.nextLine();
+        nome = read.nextLine();
+        RandomAccessFile raf = new RandomAccessFile(nome, "rw");
+        System.out.println("Digite o que deseja escrever no arquivo:");
+        String text = read.nextLine();
+        byte[] b = text.getBytes();
+        raf.read(b);
+        SecureRandom random = new SecureRandom();
+        int chave = random.nextInt(900000) + 100000;
+        System.out.println("Sua chave de criptografia eh: " + chave);
+        b = cifrar(b,chave);
+        raf.seek(0);
+        raf.writeInt(chave ^ 424242);
+        raf.write(b);
+        raf.close();
     }
 
     public static void criptografarArquivo() throws Exception{
-       Scanner read = new Scanner(System.in);
-       System.out.print("Digite o nome do arquivo: ");
-       String name = read.nextLine();
-       RandomAccessFile raf = new RandomAccessFile(name, "rw");
-       int tam = (int)raf.length();
-       byte[] b = new byte[tam];
-       raf.read(b);
-       Random random = new Random();
-       int chave = random.nextInt(100);
-       System.out.println("Sua chave de criptografia eh: " + chave);
-       b = cifrar(b, chave);
-       raf.seek(0);
-       raf.writeInt(chave);
-       raf.write(b);
-       raf.close();
-       read.close();
+        System.out.print("Digite o nome do arquivo com extensão: ");
+        String name = ""; 
+        name = read.nextLine();
+        name = read.nextLine();
+        RandomAccessFile raf = new RandomAccessFile(name, "rw");
+        int tam = (int)raf.length();
+        byte[] b = new byte[tam];
+        raf.read(b);
+        SecureRandom random = new SecureRandom();
+        int chave = random.nextInt(900000) + 100000;
+        System.out.println("Sua chave de criptografia eh: " + chave);
+        b = cifrar(b, chave);
+        raf.seek(0);
+        raf.writeInt(chave ^ 424242);
+        raf.write(b);
+        raf.close();
     }
 
     public static void descriptografarArquivo() throws Exception{
-       Scanner read = new Scanner(System.in);
-       System.out.print("Digite o nome do arquivo: ");
-       String name = read.nextLine();
-       RandomAccessFile raf = new RandomAccessFile(name, "rw");
-       int chave_correta = raf.readInt();
-       boolean chave_errada = false;
-       int chave = 0;
-       do{
-         chave_errada = false;
-         System.out.print("Qual eh a sua chave de criptografia? ");
-         chave = read.nextInt();
-         if(chave != chave_correta){
-            chave_errada = true;
-            System.out.println("Chave incompativel");
-         }
-       }while(chave_errada);
-       int tam = (int)raf.length()-4;
-       byte[] b = new byte[tam];
+        System.out.print("Digite o nome do arquivo: ");
+        String name = ""; 
+        name = read.nextLine();
+        name = read.nextLine();
+        RandomAccessFile raf = new RandomAccessFile(name, "rw");
+        int chave_correta = raf.readInt() ^ 424242;
+        boolean chave_errada = false;
+        int chave = 0;
+        do{
+            chave_errada = false;
+            System.out.print("Qual eh a sua chave de criptografia? ");
+            chave = read.nextInt();
+            if(chave != chave_correta){
+                chave_errada = true;
+                System.out.println("Chave incompativel");
+            }
+        }while(chave_errada);
+        int tam = (int)raf.length()-4;
+        byte[] b = new byte[tam];
 
-       raf.read(b);
-       b = decifrar(b, chave);
-       raf.close();
-       File f = new File(name);
-       f.delete();
-       raf = new RandomAccessFile(name, "rw");
-       raf.write(b);
-       raf.close();
-       read.close();
+        raf.read(b);
+        b = decifrar(b, chave);
+        raf.close();
+        File f = new File(name);
+        f.delete();
+        raf = new RandomAccessFile(name, "rw");
+        raf.write(b);
+        raf.close();
     }
-    
+
     public static byte[] cifrar(byte[] b, int chave){
-      for(int i = 0; i < b.length; i++){
-         b[i] = (byte)(b[i]+(byte)chave);   
-      }
-    return b;
+        for(int i = 0; i < b.length; i++){
+            b[i] = (byte)(b[i]+(byte)chave);   
+        }
+        return b;
     }
-    
+
     public static byte[] decifrar(byte[] b, int chave){
-      for(int i = 0; i < b.length; i++){
-         b[i] = (byte)(b[i]-(byte)chave);   
-      }
-     return b;
+        for(int i = 0; i < b.length; i++){
+            b[i] = (byte)(b[i]-(byte)chave);   
+        }
+        return b;
     }
-}
+}// Fim classe Projeto
